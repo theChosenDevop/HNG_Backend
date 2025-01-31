1ef6fad8f84105677d832736e7d6cf9c43abc00a
theChosenDevop <tobbey.adesanya@gmail.com>
2025-01-31 13:59:13 -0800

    fetch user detils endpoint

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        "email": "tobbey.adesanya@gmail.com",
+        "current_datetime": new Date().toISOString(),
+        "github_url": "https://github.com/theChosenDevop/HNG_12/backend",
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
